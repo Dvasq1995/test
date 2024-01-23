0b1e73f0de502ef6019a65c62fe68262b8f7627d
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2024-01-23 12:00:22 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed firefox discord steam brightnessctl lutris ttf-liberation pavucontrol atool unrar bleachbit nm-connection-editor network-manager-applet qt5ct kvantum yt-dlp cpupower xorg-xhost xdg-desktop-portal-gtk mpv qbittorrent noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nodejs npm eslint prettier fcitx5-mozc fcitx5-qt fcitx5-gtk fcitx5-configtool libreoffice-fresh drawing neofetch otf-font-awesome waybar breeze polkit-kde-agent cliphist udiskie audacity obs-studio kdenlive kde-cli-tools kcalc kdegraphics-thumbnailers kimageformats5 libheif qt5-imageformats kdesdk-thumbnailers ffmpegthumbs taglib vulkan-tools linux-headers gufw lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+pacman --sync --refresh --sysupgrade --needed discord steam lutris bleachbit yt-dlp cpupower mpv qbittorrent noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nodejs npm eslint prettier fcitx5-im fcitx5-mozc libreoffice-fresh drawing neofetch audacity obs-studio kdenlive timeshift lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
 # Set environment variables
 echo "GTK_IM_MODULE=fcitx" >> /etc/environment
 echo "QT_IM_MODULE=fcitx" >> /etc/environment
 echo "XMODIFIERS=@im=fcitx" >> /etc/environment
-echo "GDK_BACKEND=wayland,x11" >> /etc/environment
-echo "SDL_VIDEODRIVER=wayland" >> /etc/environment
-echo "CLUTTER_BACKEND=wayland" >> /etc/environment
-echo "XDG_CURRENT_DESKTOP=Hyprland" >> /etc/environment
-echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
-echo "XDG_SESSION_DESKTOP=Hyprland" >> /etc/environment
-echo "QT_AUTO_SCREEN_SCALE_FACTOR=1" >> /etc/environment
-echo "QT_QPA_PLATFORM=wayland;xcb" >> /etc/environment
-echo "QT_WAYLAND_DISABLE_WINDOWDECORATION=1" >> /etc/environment
-echo "QT_QPA_PLATFORMTHEME=qt5ct" >> /etc/environment
 
 echo "Script execution completed."
