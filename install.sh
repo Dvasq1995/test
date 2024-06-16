debcb9716b8a6e6bf3ecaa1dba09f5dbf4b1b623
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2024-06-16 01:12:46 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed chromium discord steam lutris bleachbit yt-dlp mpv qbittorrent noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fcitx5-im fcitx5-mozc libreoffice-fresh drawing neofetch audacity obs-studio kdenlive linux-headers lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader atool file perl bzip2 cpio gzip lhasa lzop p7zip tar unace unrar unzip xz zip
+pacman --sync --refresh --sysupgrade --needed chromium noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fcitx5-im fcitx5-mozc mpv bleachbit linux-headers
 
 # Set environment variables
 echo "GTK_IM_MODULE=fcitx" >> /etc/environment
