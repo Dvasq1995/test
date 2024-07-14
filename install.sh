c32b7c46f6c3ffca6d3b850e8d2b5efa0c9b9685
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2024-07-14 13:23:13 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed chromium discord noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fcitx5-im fcitx5-mozc mpv bleachbit linux-headers cliphist polkit-kde-agent
+pacman --sync --refresh --sysupgrade --needed firefox discord noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fcitx5-im fcitx5-mozc mpv bleachbit linux-headers audacity obs-studio gimp inkscape kdenlive libreoffice-fresh timeshift qbittorrent openrazer-daemon
 
 # Set environment variables
 echo "GTK_IM_MODULE=fcitx" >> /etc/environment
 echo "QT_IM_MODULE=fcitx" >> /etc/environment
 echo "XMODIFIERS=@im=fcitx" >> /etc/environment
 
+gpasswd -a $USER plugdev
 echo "Script execution completed."
