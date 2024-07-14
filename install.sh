d4e25c008518df6fc1931dd1799a55bc43ecc58b
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2024-07-14 14:00:50 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed firefox discord noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fcitx5-im fcitx5-mozc mpv bleachbit linux-headers audacity obs-studio gimp inkscape kdenlive libreoffice-fresh timeshift qbittorrent openrazer-daemon
+pacman --sync --refresh --sysupgrade --needed firefox discord noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fcitx5-im fcitx5-mozc mpv bleachbit linux-headers audacity obs-studio gimp inkscape kdenlive libreoffice-fresh timeshift qbittorrent openrazer-daemon gnome-browser-connector gnome-shell-extension-appindicator
 
 # Set environment variables
 echo "GTK_IM_MODULE=fcitx" >> /etc/environment
