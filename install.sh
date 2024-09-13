76c5134f06b24df2965527f779ef3a41a1fb7ca7
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2024-09-12 19:57:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra timeshift gnome-shell-extension-appindicator
+pacman --sync --refresh --sysupgrade --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-shell-extension-appindicator
 
 echo "Script execution completed."
