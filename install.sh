33778917ab58c67a384beb82e778d95f73a23cc6
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2024-11-23 01:38:22 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-shell-extension-appindicator
+pacman --sync --refresh --sysupgrade --needed firefox noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-shell-extension-appindicator qbittorrent
 
 echo "Script execution completed."
