33857dcf1098e770f0ed6cf4dd49d43561b5a658
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2025-02-12 13:41:51 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed firefox noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-shell-extension-appindicator gnome-browser-connector qbittorrent timeshift bleachbit mpv discord fcitx5-im fcitx5-mozc fastfetch
+pacman --sync --refresh --sysupgrade --needed firefox noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-shell-extension-appindicator gnome-browser-connector qbittorrent timeshift bleachbit mpv discord
 
 # Set environment variables
- echo "GTK_IM_MODULE=fcitx" >> /etc/environment
- echo "QT_IM_MODULE=fcitx" >> /etc/environment
- echo "XMODIFIERS=@im=fcitx" >> /etc/environment
+# echo "GTK_IM_MODULE=fcitx" >> /etc/environment
+# echo "QT_IM_MODULE=fcitx" >> /etc/environment
+# echo "XMODIFIERS=@im=fcitx" >> /etc/environment
 
 echo "Script execution completed."
