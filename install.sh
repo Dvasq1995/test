c16f15492116a679ba73c7d12745717852f4fe1a
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2025-04-14 04:07:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-browser-connector
+pacman --sync --refresh --sysupgrade --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-browser-connector gnome-shell-extension-appindicator linux-headers timeshift bleachbit qbittorrent openrazer-daemon discord chromium libreoffice-fresh
 
 # Set environment variables
 echo "GTK_IM_MODULE=fcitx" >> /etc/environment
