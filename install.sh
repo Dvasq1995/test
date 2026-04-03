90a6573b0c1bdba7e5d91a034d8aa127a7c00540
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2026-04-03 15:56:26 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam
 
 # Set environment variables
-echo "GTK_IM_MODULE=fcitx" >> /etc/environment
-echo "QT_IM_MODULE=fcitx" >> /etc/environment
-echo "XMODIFIERS=@im=fcitx" >> /etc/environment
+# echo "GTK_IM_MODULE=fcitx" >> /etc/environment
+# echo "QT_IM_MODULE=fcitx" >> /etc/environment
+# echo "XMODIFIERS=@im=fcitx" >> /etc/environment
 
 echo "Script execution completed."
