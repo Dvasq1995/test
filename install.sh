3df932d6dbb0a6d37624d056181f99e324b47001
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2026-04-14 18:41:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam
-
-# Set environment variables
-# echo "GTK_IM_MODULE=fcitx" >> /etc/environment
-# echo "QT_IM_MODULE=fcitx" >> /etc/environment
-# echo "XMODIFIERS=@im=fcitx" >> /etc/environment
+pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam ffmpegthumbnailer
 
 echo "Script execution completed."
