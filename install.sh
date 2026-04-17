129262506527d249b5d121e8e84a9a015d6e8886
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2026-04-17 10:43:41 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+pacman --sync --refresh --needed archlinux-keyring
+pacman-key --init
+pacman-key --populate
+pacman-key --refresh-keys
+
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam ffmpegthumbnailer
+pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator gnome-browser-connector chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh libreoffice-fresh-ja qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam ffmpegthumbnailer anki fcitx5-im fcitx5-mozc
+
+# Set environment variables
+echo "GTK_IM_MODULE=fcitx" >> /etc/environment
+echo "QT_IM_MODULE=fcitx" >> /etc/environment
+echo "XMODIFIERS=@im=fcitx" >> /etc/environment
 
 echo "Script execution completed."
