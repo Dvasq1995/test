7132de7a10806837b3dfff75e5a6d3a30ef3e18d
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2026-04-29 13:51:37 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ pacman-key --populate
 pacman-key --refresh-keys
 
 # Update and install packages
-pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator gnome-browser-connector chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam ffmpegthumbnailer fcitx5-im fcitx5-mozc
+pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam ffmpegthumbnailer
 
 # Set environment variables
 echo "GTK_IM_MODULE=fcitx" >> /etc/environment
