69f93f88e80b312119bed07b5785b2ed3ce4c8b0
Diego Vasquez <51815320+Dvasq1995@users.noreply.github.com>
2026-04-29 13:51:58 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ pacman-key --refresh-keys
 pacman --sync --refresh --sysupgrade --needed gnome-shell-extension-appindicator chromium discord spotify-launcher timeshift bleachbit libreoffice-fresh qbittorrent vlc vlc-plugins-all yt-dlp kdenlive audacity fastfetch noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk steam ffmpegthumbnailer
 
 # Set environment variables
-echo "GTK_IM_MODULE=fcitx" >> /etc/environment
-echo "QT_IM_MODULE=fcitx" >> /etc/environment
-echo "XMODIFIERS=@im=fcitx" >> /etc/environment
+# echo "GTK_IM_MODULE=fcitx" >> /etc/environment
+# echo "QT_IM_MODULE=fcitx" >> /etc/environment
+# echo "XMODIFIERS=@im=fcitx" >> /etc/environment
 
 pacman -Rcs $(pacman -Qdtq)
 pacman -Scc
